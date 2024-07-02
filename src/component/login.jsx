8704368693512d77d22bfa1ec0adcd52d4a605cc
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 20:34:41 +0530

    Update login.jsx

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { useState } from 'react';
-export default function Login ()
+export default function Login()
 
     {
         const[email,setEmail]=useState('');

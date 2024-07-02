15d065cbef0aebd6d16e486bc381b1fb128a5cf4
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 21:02:39 +0530

    Update error.jsx

--- src/component/error.jsx
+++ src/component/error.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 export default function Error() {
   return (
-    <div style={{backgroundColor:'black' }}>
-      <h1>Error</h1>
-       
-       
+    <div class="contents" style={{"backgroundImage":'url(error404.gif)',"color":"white","backgroundColor":"lightblack",height: '100vh',backgroundRepeat:'no-repeat'}}>
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      
+      <a href='/'><button className="Error" style={{ backgroundColor: 'lightblue', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Home</button></a>
+    
+     
       
     </div>
+</div>
   );
 }

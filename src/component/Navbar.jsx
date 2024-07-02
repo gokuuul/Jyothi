8a85e76a5f4cafdfee82b6fd7c3975104210801a
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 20:15:19 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,7 +10,7 @@ class LoginPage extends React.Component {
                 <h1 style={{ textAlign: 'center', color: '#15CDCA', fontWeight: 'bold' ,paddingTop:'20px'}}>Jyothi Engineering College</h1>
                 <div className="button-container" style={{ textAlign: 'center' , marginTop: '20px' }}>
                     <button className="student-login"  style={{ backgroundColor: '#87CDCE', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Home</button>
-                    <a href='./Login'><button  className="faculty-login"  style={{ backgroundColor: '#87CDCE', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Login</button></a>
+                    <a href='/Login'><button  className="faculty-login"  style={{ backgroundColor: '#87CDCE', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Login</button></a>
                     <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#87CDCE', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>About Us</button></a>
                     <button className="home"  style={{ backgroundColor: '#87CDCE', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>More</button>
                 </div>

da9d3835eea9478e6e25df8fd4274c3ab859017a
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 19:14:33 +0530

    Update login.jsx

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -53,7 +53,7 @@ function Login ()
                 "paddingTop": "10px"}}><button onClick={handleLogin}>login</button></div>
         <div class="d">
             <p>If you have not registered</p>
-            <a href="./signin" style={{"color":"lavender"}}>Signup</a>
+            <a href="./Sign" style={{"color":"lavender"}}>Signup</a>
         </div>
         <div>
             

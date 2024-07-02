9f60768254ed112b7660889f352816eaecf2a684
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 20:29:31 +0530

    Update error.jsx

--- src/component/error.jsx
+++ src/component/error.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Error() {
   return (
-    <div style={{ }}>
-        <img src='C:\Users\MYBOOK\Desktop\react\app\public\err.gif'></img>
+    <div style={{backgroundColor:'black' }}>
+       
        
       
     </div>

63f16d0717ec2dc6fae88eaaea01c2852e9dcc17
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 20:53:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter ,Route, Routes} from 'react-router-dom';  
-import Navbar from './component/Navbar.jsx';
+import{BrowserRouter,Route,Router,Routes} from 'react-router-dom';
+import Nav from './component/navbar.jsx'
+import AboutUs from './component/about.jsx';
+import SignUp from './component/signup.jsx';
 import Login from './component/login.jsx';
-import Sign from './component/signin.jsx'
-import About from './component/About.jsx';
 import Error from './component/error.jsx';
-
-
+import Contactus from './component/contactus.jsx';
 function App() {
   return (
-    <>
+    
     
     <BrowserRouter>
-      <Routes>
-        <Route path='/Sign' element={<Sign />}></Route>
-        <Route path='/Login' element={<Login />}></Route>
-        <Route path='/About' element={<About />}></Route>
-        <Route path='/' element={<Navbar />}></Route>
-        <Route path='/*' element={<Error />}></Route>
-        
-        
-
-
-      </Routes>
-      </BrowserRouter>
-      </>
-
+    <Routes>
+      <Route path='/Signup' element={<SignUp />}></Route>
+      <Route path='/Login' element={<Login />}></Route>
+      <Route path='/Aboutus' element={<AboutUs />}></Route>
+      <Route path='/Contactus' element={<Contactus />}></Route>
+      <Route path='/' element={<Nav />}></Route>
+      <Route path='/*' element={<Error />}></Route>
+    </Routes>
+    </BrowserRouter>
+  
   );
 }
 

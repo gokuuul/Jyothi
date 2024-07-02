f4cd17abb40d2a3f8b0f7b50cfd29c9493b66c79
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 21:45:31 +0530

    Update error.jsx

--- src/component/error.jsx
+++ src/component/error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Error() {
   return (
-    <div class="contents" style={{"backgroundImage":'url(D.jpg)',"color":"white","backgroundColor":"lightblack",height: '100vh',backgroundRepeat:'no-repeat'}}>
+    <div class="contents" style={{"backgroundImage":'url(E.jpg)',"color":"white","backgroundColor":"lightblack",height: '100vh',backgroundRepeat:'no-repeat'}}>
     <div style={{ textAlign: 'center', padding: '50px' }}>
       
       <a href='/'><button className="Error" style={{ backgroundColor: 'lightblue', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Home</button></a>

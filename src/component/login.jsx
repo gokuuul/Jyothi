84a77b078b02d84905ff0d95cd94f1b9076f4974
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-07-02 18:47:20 +0530

    Update login.jsx

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
-import axios  from 'axios';
+import axios from 'axios';
 import { useState } from 'react';
-
 function Login ()
+
     {
         const[email,setEmail]=useState('');
         const[pass1,setPass1]=useState('');
-
         function handleChangeemail(e){
             setEmail(e.target.value)
         }
 
-        function handleChangepass(e){
-            setPass1(e.target.value)
-        }
-      async  function handleLogin(){
-            const response=  await axios.post('http://localhost:5000/login',{
-                email,
-                pass1
+            function handleChangepass1(e){
+                setPass1(e.target.value)
+            }
+          async function handleLogin(){ 
+                const response=await axios.post('http://localhost:5000/Login',{
+                    email,
+                    pass1
+
             })
             console.log(response);
-            
         }
+            
+            
+        
     return(
-        <div class="contents" style={{"backgroundImage":'url(D.jpg)',height:'100vh'}}>
+        <div class="contents" style={{"backgroundImage":'url(lala.jpg)',"color":"white",height: '100vh'}}>
         <div class="content" style={{"display": "list-item",
                 "justifyContent": "center",
-                "paddingLeft": "40%",
+                marginInline:'auto',
+            
                 "width": "max-content",
                 "position": "relative",
                 "align": "center",
-                "paddingTop": "30px"}}>
+                "paddingTop": "30px,"}}>
         <h1>LOGIN </h1>
         <div class="a" style={{"padding": "auto",
                 "display": "flex",
@@ -44,13 +47,13 @@ function Login ()
                 "paddingTop": "10px",
                 "justifyContent": "space-between"}}>
             <label for="">Password</label>
-            <input type="password" placeholder="Enter the password" onChange={handleChangepass} />
+            <input type="password" placeholder="Enter the password" onChange={handleChangepass1} />
         </div>
         <div class="c" style={{ "paddingLeft": "230px",
                 "paddingTop": "10px"}}><button onClick={handleLogin}>login</button></div>
         <div class="d">
             <p>If you have not registered</p>
-            <a href="/Sign" style={{"color":"black"}}>signup</a>
+            <a href="./Signup" style={{"color":"lavender"}}>Signup</a>
         </div>
         <div>
             
